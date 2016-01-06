81a824d8b485320c0ea39c4cfaab866376c6d503
Boaz Rosenan <brosenan@gmail.com>
2016-01-06 12:42:34 +0200

    moved to local testing

--- test/objStore-test.js
+++ test/objStore-test.js
@@ -9,11 +9,8 @@ var request = require('request');
 
 var Nodalion = require('nodalion');
 require('../objStore.js').configure({
-    provider: 'amazon',
-    protocol: 'http://',
-    serversUrl: 'fakes3',
-    accessKeyId: 'ACCESS_KEY_ID',
-    accessKey: 'SECRET_ACCESS_KEY',
+    provider: 'filesystem',
+    root: '/tmp',
     container: 'objStoreTest',
 });
 
@@ -25,68 +22,77 @@ var doTask = function(task, cb) {
     task.meaning()(nodalion, cb);
 };
 
-describe('/nodalion#objStoreAdd(Str)', function(){
-    it('should turn a string into a hash', $T(function*(){
-	var hash = yield doTask(ns.objStoreAdd("Hello, World\n"), $R());
-	var str = yield objStore.getString(hash, $R());
-	assert.equal(str, 'Hello, World\n');
-    }));
-});
-
-describe('/nodalion#objStoreCat(Hash)', function(){
-    it('should turn a hash into a string', $T(function*(){
-	var hash = yield objStore.addString("Hello, World\n", $R());
-	var str = yield doTask(ns.objStoreCat(hash), $R());
-	assert.equal(str, "Hello, World\n");
-    }));
-});
-
-describe('/nodalion#objStoreToTmp(Hash)', function(){
-    it('should create a temporary file with the content under Hash and return its path', $T(function*(){
-	var hash = yield objStore.addString("Hello, World\n", $R());
-	var path = yield doTask(ns.objStoreToTmp(hash), $R());
-	var reader = fs.createReadStream(path);
-	var result = '';
-	reader.setEncoding('utf-8');
-	reader.on('data', data => { result += data; });
-	yield reader.on('end', $RR());
-	assert.equal(result, 'Hello, World\n');
-    }));
-
-});
-
-describe('/nodalion#objStoreBody', function(){
+describe('nodalion-objstore', function(){
     before($T(function*() {
-//	var impred = Nodalion.namespace('/impred', ['pred']);
-//	var builtin = Nodalion.namespace('builtin', ['true']);
-//	yield nodalion.findAll([], impred.pred(builtin.true()), $R());
+	fs.mkdirSync('/tmp/objStoreTest');
+    }));
+    after($T(function*() {
+	yield require('child_process').spawn('rm', ['-rf', '/tmp/objStoreTest']).on('exit', $RR());
+    }));
+    describe('/nodalion#objStoreAdd(Str)', function(){
+	it('should turn a string into a hash', $T(function*(){
+	    var hash = yield doTask(ns.objStoreAdd("Hello, World\n"), $R());
+	    var str = yield objStore.getString(hash, $R());
+	    assert.equal(str, 'Hello, World\n');
+	}));
+    });
 
-	var app = express();
-	app.use(nodalionHttp.app(nodalion, example.myObjStoreApp()));
-	app.listen(3002);
-	yield setTimeout($R(), 10); // Give the app time to go up
-    }));
-    it('should handle objStore content requests', $T(function*(){
-	var hash = yield objStore.addString('Hello, World\n', $R());
-	var resp = yield request('http://localhost:3002/objStore/' + hash, $RR());
-	assert.ifError(resp[0]);
-	assert.equal(resp[1].statusCode, 200);
-	assert.equal(resp[1].headers['content-type'].split(';')[0], 'text/foo');
-	assert.equal(resp[2], 'Hello, World\n');
-    }));
-    it('should handle objStore add requests', $T(function*(){
-	var resp = yield request({
-	    url: 'http://localhost:3002/objStore',
-	    method: 'POST',
-	    headers: {'content-type': 'text/foo'},
-	    body: 'Hello, World\n',
-	}, $RR());
-	assert.ifError(resp[0]);
-	assert.equal(resp[1].statusCode, 200);
-	var url = JSON.parse(resp[2]).url;
-	resp = yield request(url, $RR());
-	assert.ifError(resp[0]);
-	assert.equal(resp[1].statusCode, 200);
-	assert.equal(resp[2], 'Hello, World\n');
-    }));
+    describe('/nodalion#objStoreCat(Hash)', function(){
+	it('should turn a hash into a string', $T(function*(){
+	    var hash = yield objStore.addString("Hello, World\n", $R());
+	    var str = yield doTask(ns.objStoreCat(hash), $R());
+	    assert.equal(str, "Hello, World\n");
+	}));
+    });
+
+    describe('/nodalion#objStoreToTmp(Hash)', function(){
+	it('should create a temporary file with the content under Hash and return its path', $T(function*(){
+	    var hash = yield objStore.addString("Hello, World\n", $R());
+	    var path = yield doTask(ns.objStoreToTmp(hash), $R());
+	    var reader = fs.createReadStream(path);
+	    var result = '';
+	    reader.setEncoding('utf-8');
+	    reader.on('data', data => { result += data; });
+	    yield reader.on('end', $RR());
+	    assert.equal(result, 'Hello, World\n');
+	}));
+
+    });
+
+    describe('/nodalion#objStoreBody', function(){
+	before($T(function*() {
+	    var impred = Nodalion.namespace('/impred', ['pred']);
+	    var builtin = Nodalion.namespace('builtin', ['true']);
+	    yield nodalion.findAll([], impred.pred(builtin.true()), $R());
+
+	    var app = express();
+	    app.use(nodalionHttp.app(nodalion, example.myObjStoreApp()));
+	    app.listen(3002);
+	    yield setTimeout($R(), 10); // Give the app time to go up
+	}));
+	it('should handle objStore content requests', $T(function*(){
+	    var hash = yield objStore.addString('Hello, World\n', $R());
+	    var resp = yield request('http://localhost:3002/objStore/' + hash, $RR());
+	    assert.ifError(resp[0]);
+	    assert.equal(resp[1].statusCode, 200);
+	    assert.equal(resp[1].headers['content-type'].split(';')[0], 'text/foo');
+	    assert.equal(resp[2], 'Hello, World\n');
+	}));
+	it('should handle objStore add requests', $T(function*(){
+	    var resp = yield request({
+		url: 'http://localhost:3002/objStore',
+		method: 'POST',
+		headers: {'content-type': 'text/foo'},
+		body: 'Hello, World\n',
+	    }, $RR());
+	    assert.ifError(resp[0]);
+	    assert.equal(resp[1].statusCode, 200);
+	    var url = JSON.parse(resp[2]).url;
+	    resp = yield request(url, $RR());
+	    assert.ifError(resp[0]);
+	    assert.equal(resp[1].statusCode, 200);
+	    assert.equal(resp[2], 'Hello, World\n');
+	}));
+    });
+    
 });
